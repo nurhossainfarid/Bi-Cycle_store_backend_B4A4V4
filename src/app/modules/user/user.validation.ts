9c0fdfe5b24cd51ffc6f757a8e2898bff83c1798
nurhossainfarid <faahsan1516@gmail.com>
2025-01-26 10:51:08 +0600

    create user module

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -2,10 +2,10 @@ import { z } from 'zod'
 
 export const createUserValidationSchema = z.object({
   body: z.object({
-    author: z.object({
+    user: z.object({
       email: z.string().email(),
       password: z.string().min(8),
       name: z.string().min(2),
     }),
   }),
-})
+});
